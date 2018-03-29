a13cd47419ddbfbd571bbb895cf778c27d241843
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-29 08:31:09 -0400

    add account page

---
+++ src/pages/accounts/accounts.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'accounts',
+  templateUrl: 'accounts.html'
+})
+export class AccountsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
