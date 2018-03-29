45227a6293df4e9112e3795b335110c7dd86eafa
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-29 14:05:10 -0400

    generated page

--- src/pages/accounts/accounts.ts
+++ src/pages/accounts/accounts.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the AccountsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
-  selector: 'accounts',
-  templateUrl: 'accounts.html'
+  selector: 'page-accounts',
+  templateUrl: 'accounts.html',
 })
+
 export class AccountsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AccountsPage');
   }
 
   addAccount() {
@@ -25,7 +37,5 @@ export class AccountsPage {
   checkIfAuthenticated(): Boolean {
     return true;
   }
-
-
 
 }
