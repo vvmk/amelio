cb40a1f26b7213031d4343cb55e4417040165f14
vvmk <vincentmasiello@gmail.com>
2018-03-30 12:59:15 -0400

    put back firebase import

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 import { UserServiceProvider } from '../../providers/user-service/user-service';
 
