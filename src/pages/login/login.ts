5851ba426e0f53faf240985e1a862701282d0d33
vvmk <vincentmasiello@gmail.com>
2018-03-30 12:12:12 -0400

    Attach 'register' submit button

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,55 +1,53 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase/app';
 
 import { UserServiceProvider } from '../../providers/user-service/user-service';
 
-@IonicPage()
-@Component({
-	selector: 'page-login',
-	templateUrl: 'login.html',
-})
-export class LoginPage {
-	method: string = "login";
+ @IonicPage()
+ @Component({
+ 	selector: 'page-login',
+ 	templateUrl: 'login.html',
+ })
+ export class LoginPage {
+ 	method: string = "login";
 
-	login = {
-		email: '',
-		password: ''
-	};
+ 	login = {
+ 		email: '',
+ 		password: ''
+ 	};
 
-	register = {
-		email: '',
-		password: '',
-		confirm: ''
-	};
+ 	register = {
+ 		email: '',
+ 		password: '',
+ 		confirm: ''
+ 	};
 
-	auth = firebase.auth;
+ 	auth = firebase.auth;
 
-	constructor(public navCtrl: NavController,
-		public navParams: NavParams,
-		public alertController: AlertController,
-		public userService: UserServiceProvider) {
+ 	constructor(public navCtrl: NavController,
+ 		public navParams: NavParams,
+ 		public userService: UserServiceProvider) {
 
 
-	}
+ 	}
 
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad LoginPage');
-	}
+ 	ionViewDidLoad() {
+ 		console.log('ionViewDidLoad LoginPage');
+ 	}
 
-	submitLogin() {
-		if (!this.login.email || !this.login.password) {
-			this.userService.displayAlert("Empty Field", "Please fill out all the fields.");
-		} else {
-			console.log(`email: ${ this.login.email }, password: ${ this.login.password }`);
-		}
-	}
+ 	submitLogin() {
+ 		if (!this.login.email || !this.login.password) {
+ 			this.userService.displayAlert("Empty Field", "Please fill out all the fields.");
+ 		}
+ 		console.log(this.login);
+ 	}
 
-	submitCreateAccount() {
-		if (this.register.password != this.register.confirm) {
-			this.userService.displayAlert("Password mismatch", "Try again, bozo.");
-			this.register.password = '';
-			this.register.confirm = '';
+ 	submitCreateAccount() {
+ 		if (this.register.password != this.register.confirm) {
+ 			this.userService.displayAlert("Password mismatch", "Try again, bozo.");
+ 			this.register.password = '';
+ 			this.register.confirm = '';
  		} else { // all is well
  			console.log("fields good, creating user");
  		}
