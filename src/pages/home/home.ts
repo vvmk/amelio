cfcb5221456a20eee153dfa0679774892adc9030
Amy Gill <amy.gill.34@gmail.com>
2018-04-01 18:08:45 -0400

    hard-coded homepage tiles stubbed out

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,10 +6,26 @@ import { IonicPage, NavController } from 'ionic-angular';
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
+
+
 export class HomePage {
+	/*"declare a field variable called message of type string"
+	message: string = "hello";
+	*/
+
+	//"make an array of strings called messages"
+	messages: Card[] = [];
+
 
 	constructor(public navCtrl: NavController) {
+		//adding data (items) to our array
 
+		this.messages.push(new Card("Pluralsight", "Here is how many hours of PluralSight you have completed today:"));
+		this.messages.push(new Card("Codewars", "Look at all of the badges you earned in Codewars!"));
+		this.messages.push(new Card("Codeschool", "Here is your current learning path progress:"));
+
+		//this.messages.push("","test string info. have a nice day");
+		
 	}
 
 	openShareModal() {
@@ -19,5 +35,15 @@ export class HomePage {
 
 	showAccountsPage() {
     	this.navCtrl.push('AccountsPage');
+	}
+}
+
+class Card{
+	header:string;
+	message:string;
+
+	constructor(header:string, message:string){
+		this.header = header;
+		this.message = message;
 	}
 }
